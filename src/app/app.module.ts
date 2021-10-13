5cb243796646699aef315b22c841eb08fe2a4f16
Jution Candra Kirana <jutionck@gmail.com>
2021-10-13 21:07:39 +0700

    #4 Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleModule } from './sample/sample.module';
-import { ResumeModule } from './resume/resume.module';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +13,6 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SampleModule,
-    ResumeModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
