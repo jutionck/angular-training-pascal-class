0e5154682ee284295d3fed3c0ed9f44dafcb1762
Jution Candra Kirana <jutionck@gmail.com>
2021-10-14 09:50:51 +0700

    #4 Angular Routing with Lazy Load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -15,17 +13,11 @@ const routes: Routes = [
       .then(module => module.DemoModule)
   },
   {
-    path: 'contact',
-    component: ContactComponent
+    path: '',
+    loadChildren: () => import('./pages/pages.module')
+      .then(m => m.PagesModule)
   },
-  {
-    path: 'about',
-    component: AboutComponent
-  }
 ];
-
-// eager load 
-// lazy load
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
