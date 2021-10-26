634a6506283173b146beceeeb026c145315fb333
Jution Candra Kirana <jutionck@gmail.com>
2021-10-26 17:00:22 +0700

    #10 HTPP Service and Inteceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleModule } from './sample/sample.module';
@@ -9,6 +9,7 @@ import { SharedModule } from './shared/shared.module';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AuthModule } from './auth/auth.module';
+import { RequestInterceptor } from './shared/inteceptors/request.interceptor';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,9 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
